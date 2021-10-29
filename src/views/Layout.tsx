6610ebcd96f5a346a1d91463d4ae29ff90327859
Thomas <thomas@barrial.fr>
2021-10-29 14:57:54 +0200

    add tw rules and logo

--- src/views/Layout.tsx
+++ src/views/Layout.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import logo from '../assets/logo.svg';
 
 function Layout(): JSX.Element {
   return (
-    <div className="h-screen w-screen flex items-center justify-center text-5xl bg-blue-500 text-white font-bold">
-      HelloWorld
+    <div className="h-screen w-screen flex flex-col items-center justify-center text-5xl bg-darkBlue font-lexend text-white font-bold">
+      <img className="mb-32" src={logo} alt="" />
     </div>
   );
 }
