fc78f760dee09c58fa8a40ab76af91075cde715a
Manon Trefoux <manon.trefoux@gmail.com>
2022-01-18 16:15:45 +0100

    feat: add TaskList component

--- src/API/queries/taskQueries.ts
+++ src/API/queries/taskQueries.ts
@@ -1,5 +1,18 @@
 import { gql } from '@apollo/client';
 
+export const GetAllTasks = gql`
+  query GetAllTasks {
+    getAllTasks {
+      id
+      subject
+      startDate
+      endDate
+      estimeeSpentTime
+      advancement
+    }
+  }
+`;
+
 export const GetOneTask = gql`
   query getTaskByID($getTaskByIdId: String!) {
     getTaskByID(id: $getTaskByIdId) {
