beace0026c306bc9297ad5266e940fed94ea9115
Manon Trefoux <manon.trefoux@gmail.com>
2022-01-18 15:23:56 +0100

    feat:add ProjectOwner component

---
+++ src/components/projects/ProjectOwner.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import dfltAvatar from '../../assets/DefaultAvatar.webp';
+
+function ProjectOwner(): JSX.Element {
+  return (
+    <div className="flex items-center float-right">
+      <div
+        className="h-24 lg:h-8 w-24 lg:w-8 rounded-full border-4 lg:border-2 border-purple"
+        style={{
+          backgroundImage: `url(${dfltAvatar})`,
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
+        }}
+      />
+      <div className="ml-2">
+        <p className="text-xs text-lightPurple">Project Owner</p>
+        <p className="text-xs">Nom du Project Owner</p>
+      </div>
+    </div>
+  );
+}
+
+export default ProjectOwner;
--- src/views/projects/ProjectDetails.tsx
+++ src/views/projects/ProjectDetails.tsx
@@ -3,6 +3,7 @@ import { useQuery } from '@apollo/client';
 import { RouteComponentProps } from 'react-router-dom';
 
 import * as queries from '../../API/queries/projectQueries';
+import ProjectOwner from '../../components/projects/ProjectOwner';
 
 function ProjectDetails({
   match,
@@ -20,19 +21,24 @@ function ProjectDetails({
   }
 
   return (
-    <div className="lg:flex justify-between mt-9">
+    <>
       {data && (
-        <div className="lg:w-1/2">
-          <h1
-            style={{ borderBottom: '2px solid #8790E0' }}
-            className="text-xl text-lightPurple border-2 border-transparent pb-2 w-full"
-          >
-            PROJECT {data.getProjectByID.name.toUpperCase()}
-          </h1>
-          <p className="mt-5">{data.getProjectByID.description}</p>
+        <div className="lg:flex justify-between mt-9">
+          <div className="lg:w-1/2">
+            <h1
+              style={{ borderBottom: '2px solid #8790E0' }}
+              className="text-xl text-lightPurple border-2 border-transparent pb-2 w-full"
+            >
+              PROJECT {data.getProjectByID.name.toUpperCase()}
+            </h1>
+            <p className="mt-5">{data.getProjectByID.description}</p>
+          </div>
+          <div className="lg:w-1/2">
+            <ProjectOwner />
+          </div>
         </div>
       )}
-    </div>
+    </>
   );
 }
 export default ProjectDetails;
