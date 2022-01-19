81bf55ad3fefdc18c2b59bf4740527e2c4f8c9cf
louisledoux <19322697+louisledoux@users.noreply.github.com>
2022-01-19 10:38:05 +0100

    fix: remove dotenv and fix env variable

--- src/API/client.ts
+++ src/API/client.ts
@@ -1,11 +1,9 @@
-import dotenv from 'dotenv';
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
-dotenv.config();
-
 export const client = new ApolloClient({
-  // TODO: REMOVE LOCALHOST WHEN APP WILL GO TO PRODUCTION
-  uri: process.env.BACKEND_URL || 'http://localhost:4000/graphql',
+  uri:
+    (import.meta.env.VITE_BACKEND_URL as string) ||
+    'http://localhost:4000/graphql',
   credentials: 'include',
   cache: new InMemoryCache(),
 });
