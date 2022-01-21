07b6fcb59858d2c75c6cb240674845f9d5e8bf67
Thomas <thomas@barrial.fr>
2022-01-21 15:38:28 +0100

    fix view bug

--- src/views/tasks/TaskList.tsx
+++ src/views/tasks/TaskList.tsx
@@ -2,9 +2,9 @@ import React, { useState } from 'react';
 import { useQuery } from '@apollo/client';
 
 import HeaderList from '@components/navigation/HeaderList';
-import * as queries from '@api/queries/taskQueries';
 import OneTask from '@components/tasks/OneTask';
 import { GetAllTasks_getAllTasks } from '@api/types/GetAllTasks';
+import { GET_ALL_TASKS } from '@api/queries/taskQueries';
 import CreateUpdateTask from './CreateUpdateTask';
 
 interface IResponse {
@@ -12,49 +12,31 @@ interface IResponse {
 }
 
 function TaskList(): JSX.Element {
-  const [data, setData] = useState<GetAllTasks_getAllTasks[]>([]);
   const [isModal, setIsModal] = useState(false);
   // FETCH THE TASK LIST
-  const { loading, error } = useQuery<IResponse>(queries.GET_ALL_TASKS, {
-    onCompleted: (d) => {
-      setData(d.getAllTasks);
-    },
-  });
-
-  // ON CREATED TASK ! ADD THE CREATED TASK TO THE ARRAY
-  const onTaskCreated = (p: GetAllTasks_getAllTasks) => {
-    setData([...data, p]);
-    window.location.reload();
-  };
-
-  // REVERSE THE ARRAY THE RENDER THE YOUNGER ONE IN FIRST
-  const reverseData = [...data].reverse();
+  const { loading, error, data } = useQuery<IResponse>(GET_ALL_TASKS);
 
   if (loading) {
     return <p>...loading</p>;
   }
-  if (error) {
+  if (error || !data) {
     return <p>error</p>;
   }
   return (
     <div className="py-5 lg:py-0 lg:pt-28">
       <HeaderList setIsModal={setIsModal} name="Tasks list" />
       {isModal && (
-        <CreateUpdateTask
-          taskId={undefined}
-          setIsModal={setIsModal}
-          onTaskCreated={onTaskCreated}
-        />
+        <CreateUpdateTask taskId={undefined} setIsModal={setIsModal} />
       )}
       <div className="mt-2 px-3 lg:pr-6">
-        {reverseData.map((item) => {
+        {[...data?.getAllTasks].reverse().map((item) => {
           return (
             <div key={item.id}>
               <OneTask item={item} />
             </div>
           );
         })}
-        {data.length === 0 && (
+        {[...data?.getAllTasks].reverse().length === 0 && (
           <p className="font-normal lg:py-5 py-2 text-purple">
             There is no taks for now
           </p>
