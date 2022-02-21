772adcd3787f4c7fb74f457c0217fe18a9d1fe03
clnmlg <colin.mlg@posteo.net>
2022-02-21 17:15:45 +0100

    feat: responsive signup (to be continued)

--- src/views/Signup.tsx
+++ src/views/Signup.tsx
@@ -1,18 +1,31 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import UserCard from '@assets/User_card.png';
 import MailInput from '@components/formInput/MailInput';
 import PasswordInput from '@components/formInput/PasswordInput';
 
 function Signup(): JSX.Element {
   const { register } = useForm();
   return (
-    <div>
-      <div className="md:flex align-center mt-40 ml-10">
-        {/* <div className="flex-1 p-7">
-        </div> */}
-        <div className="flex-1 mt-2 p-7 ">
-          <div className="block p-7 rounded-lg bg-darkGray max-w-sm shadow-purple border border-gray-600">
-            <div className="text-center text-4xl font-extralight">Hi There</div>
+    <div className="flex items-center justify-center h-screen w-full">
+      <div className="md:flex gap-20 w-10/12 lg:w-7/12">
+        <div className="m-auto">
+          <div className="text-7xl bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 mb-5 drop-shadow-lg">
+            Welcome to Stack.
+          </div>
+          <div className="text-justify bg-clip-text text-lightGray mw-sm">
+            Get your work done. Stack helps you to manage your projects, create
+            tasks, and many more.
+          </div>
+        </div>
+        <div className="absolute z-0 right-10 hidden lg:flex">
+          <img src={UserCard} alt="userCard" />
+        </div>
+        <div className="justify-center z-10">
+          <div className="m-auto lg:m-0 block p-8 rounded-lg bg-darkGray w-96 shadow-purple border border-gray-600">
+            <div className="text-center text-4xl font-extralight drop-shadow-md">
+              Hi There
+            </div>
             <form>
               <MailInput
                 label=""
@@ -52,7 +65,7 @@ function Signup(): JSX.Element {
                 Signup
               </button>
               <div
-                className="mt-4 text-center font-extralight"
+                className="mt-4 text-center font-extralight drop-shadow-md"
                 style={{ color: ' #8560EE' }}
               >
                 Already have an account? Sign in
@@ -61,16 +74,6 @@ function Signup(): JSX.Element {
           </div>
         </div>
       </div>
-      {/* <div>
-        <div className="text-center mt-10 space-y-8">
-        </div>
-      </div>
-      <div className="text-center mt-36 space-y-8">
-        <div className="w-screen h-screen bg-darkBlue z-50 p-5">
-          <div className="text-center max-w-md">
-          </div>
-        </div>
-      </div> */}
     </div>
   );
 }
