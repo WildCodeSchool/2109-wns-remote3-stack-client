3565759922e681ff504e9700af9ba121230f4f3f
clnmlg <colin.mlg@posteo.net>
2022-02-22 10:38:04 +0100

    feat: loginpage & signuppage front responsive ok

--- src/views/Signup.tsx
+++ src/views/Signup.tsx
@@ -1,26 +1,33 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
-import UserCard from '@assets/User_card.png';
+// import UserCard from '@assets/User_card.png';
 import MailInput from '@components/formInput/MailInput';
 import PasswordInput from '@components/formInput/PasswordInput';
+import { useHistory } from 'react-router-dom';
 
 function Signup(): JSX.Element {
+  const history = useHistory();
   const { register } = useForm();
+
+  function goToLogin() {
+    history.push('/login');
+  }
+
   return (
     <div className="flex items-center justify-center h-screen w-full">
       <div className="md:flex gap-20 w-10/12 lg:w-7/12">
         <div className="m-auto">
-          <div className="text-7xl bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 mb-5 drop-shadow-lg">
+          <div className="sm:text-7xl text-4xl text-center group-hover:text-center bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 mb-5 drop-shadow-lg">
             Welcome to Stack.
           </div>
-          <div className="text-justify bg-clip-text text-lightGray mw-sm">
+          <div className="text-justify bg-clip-text hidden md:flex text-lightGray ">
             Get your work done. Stack helps you to manage your projects, create
             tasks, and many more.
           </div>
         </div>
-        <div className="absolute z-0 right-10 hidden lg:flex">
+        {/* <div className="absolute z-0 right-2 hidden lg:flex">
           <img src={UserCard} alt="userCard" />
-        </div>
+        </div> */}
         <div className="justify-center z-10">
           <div className="m-auto lg:m-0 block p-8 rounded-lg bg-darkGray w-96 shadow-purple border border-gray-600">
             <div className="text-center text-4xl font-extralight drop-shadow-md">
@@ -64,12 +71,14 @@ function Signup(): JSX.Element {
               >
                 Signup
               </button>
-              <div
-                className="mt-4 text-center font-extralight drop-shadow-md"
+              <button
+                type="submit"
+                onClick={goToLogin}
+                className="mt-4 ml-6 text-center font-extralight drop-shadow-md"
                 style={{ color: ' #8560EE' }}
               >
-                Already have an account? Sign in
-              </div>
+                Already have an account? Login in
+              </button>
             </form>
           </div>
         </div>
